2338fe9c2abe89a676d27ddc53620e7e52a57500
ayyaztech <ayyaz.zaffar@gmail.com>
2024-06-09 00:31:50 +0500

    chore: Update project and dependencies

--- android/app/src/main/java/com/tempproject/MainApplication.kt
+++ android/app/src/main/java/com/tempproject/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.donationapp
+package com.tempproject
 
 import android.app.Application
 import com.facebook.react.PackageList
--- ios/TempProjectTests/TempProjectTests.m
+++ ios/TempProjectTests/TempProjectTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface DonationAppTests : XCTestCase
+@interface TempProjectTests : XCTestCase
 
 @end
 
-@implementation DonationAppTests
+@implementation TempProjectTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
